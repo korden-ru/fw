1538d9e2e13101abae1c60a4897aff8f29f29322
Sergey Pankov <vacuum@ivacuum.ru>
2012-04-21 17:06:28 +0400

    Альтернативное именование методов логгера

--- core/logger.php
+++ core/logger.php
@@ -16,8 +16,59 @@ use Monolog\Logger as base_logger;
 */
 class logger extends base_logger
 {
+	/**
+	* Название уровня логирования
+	*/
+	public function get_level_name($level)
+	{
+		return $this->getLevelName($level);
+	}
+	
+	/**
+	* Название канала
+	*/
+	public function get_name()
+	{
+		return $this->getName();
+	}
+	
+	/**
+	* Проверка: обрабатываются ли события определенного уровня
+	*/
+	public function is_handling($level)
+	{
+		return $this->isHandling($level);
+	}
+	
+	/**
+	* Извлечение обработчика
+	*/
+	public function pop_handler()
+	{
+		return $this->popHandler();
+	}
+	
+	/**
+	* Извлечение процессора
+	*/
+	public function pop_processor()
+	{
+		return $this->popProcessor();
+	}
+	
+	/**
+	* Добавление обработчика в верхушку стека
+	*/
     public function push_handler($handler)
     {
 		$this->pushHandler($handler);
     }
+	
+	/**
+	* Добавление процессора в верхушку стека
+	*/
+	public function push_processor($callback)
+	{
+		$this->pushProcessor($callback);
+	}
 }
