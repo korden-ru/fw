31a7bdfb12e5d5d75d632a130e59ceaedd398314
apat1k <vk@korden.ru>
2012-08-14 16:30:43 +0300

    Улучшения в валидации электронной почты

--- form/field/email.php
+++ form/field/email.php
@@ -13,12 +13,21 @@ class email extends generic
 {
 	public function validate()
 	{
+		if( $this->data['field_disabled'] || $this->data['field_readonly'] )
+		{
+			return true;
+		}
+		
 		if( $this->data['field_required'] && !$this->data['value'] )
 		{
 			return false;
 		}
 		
-		if( !preg_match(sprintf('#%s#', get_preg_expression('email')), $this->data['value'], $matches) )
+		if( $this->data['field_pattern'] && !preg_match(sprintf('#%s#', $this->data['field_pattern']), $this->data['value']) )
+		{
+			return false;
+		}
+		elseif( !$this->data['field_pattern'] && !preg_match(sprintf('#%s#', get_preg_expression('email')), $this->data['value']) )
 		{
 			return false;
 		}
