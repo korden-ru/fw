af6466ad3c9a06a5316f1f2fe1ffd336516cd46f
Sergey Pankov <vacuum@ivacuum.ru>
2012-08-14 11:25:07 +0400

    Предварительный список поддерживаемых полей

---
+++ form/field/number.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* @package fw.korden.net
+* @copyright (c) 2012
+*/
+
+namespace engine\form\field;
+
+/**
+* Число
+*/
+class number extends generic
+{
+}
---
+++ form/field/range.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* @package fw.korden.net
+* @copyright (c) 2012
+*/
+
+namespace engine\form\field;
+
+/**
+* Ползунок
+*/
+class range extends generic
+{
+}
