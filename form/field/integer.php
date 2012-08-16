920cf63575a2ee8ac9722311905fbac6072a7f14
Sergey Pankov <vacuum@ivacuum.ru>
2012-08-16 16:29:57 +0400

    Добавление: поле для ввода целых чисел

---
+++ form/field/integer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+* @package fw.korden.net
+* @copyright (c) 2012
+*/
+
+namespace engine\form\field;
+
+/**
+* Число
+*/
+class integer extends generic
+{
+	public function validate()
+	{
+		if( $this->data['field_disabled'] || $this->data['field_readonly'] )
+		{
+			return true;
+		}
+		
+		if( $this->data['field_required'] && !$this->data['value'] )
+		{
+			return false;
+		}
+		
+		return true;
+	}
+}
