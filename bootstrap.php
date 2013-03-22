0b7c0c8034acf478041dfeba6435d67071eeca4b
Sergey Pankov <vacuum@ivacuum.ru>
2013-03-22 16:31:14 +0400

    Рефакторинг

--- bootstrap.php
+++ bootstrap.php
@@ -32,13 +32,9 @@ $app['autoloader']->register_namespaces([
 		'fw'  => [$app['dir.fw'], $app['dir.korden_fw']],
 		'app' => $app['dir.app'],
 	])
-	->register_pears([
-		'Swift' => "{$app['dir.lib']}/swiftmailer/{$app['version.swift']}/classes",
-	])
+	->register_pear('Swift', "{$app['dir.lib']}/swiftmailer/{$app['version.swift']}/classes")
 	->set_namespace_prefix('korden.fw', $app::VERSION)
-	->set_pear_prefixes([
-		'Swift' => $app['version.swift'],
-	]);
+	->set_pear_prefix('Swift', $app['version.swift']);
 
 require(KORDEN_FW_DIR . 'constants.php');
 require(SITE_DIR . '../constants.php');
